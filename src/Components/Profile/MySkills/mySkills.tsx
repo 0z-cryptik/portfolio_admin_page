502cada70521c7cbd50a44a9e7ac6f37b6ebbf73
Eniitan <enitanolawale0@gmail.com>
2026-07-09 20:33:36 +0100

    setup profile skill deletion

--- src/Components/Profile/MySkills/mySkills.tsx
+++ src/Components/Profile/MySkills/mySkills.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import { useMyGlobalState } from "../../../Context/portfolioContext";
 import { AddSkillButton } from "./addSkillButton";
+import { MdOutlineCancel } from "react-icons/md";
+import { updateBackendData } from "../../../helperFunctions/upDateBackendData";
 
 export function MySkills() {
-  const { profile } = useMyGlobalState();
+  const { profile, setProfile } = useMyGlobalState();
+  const [deletingSkill, setDeletingSkill] = useState(false);
+
+  async function handleDelete(skill: string) {
+    setDeletingSkill(true);
+    const updatedProfile = await updateBackendData(
+      "http://localhost:3000/api/profile/1/skills",
+      { skillName: skill },
+      "DELETE"
+    );
+    console.log(updatedProfile);
+    setProfile(updatedProfile);
+    setDeletingSkill(false);
+  }
 
   return (
     <div className="space-y-4 pt-10">
@@ -10,13 +26,23 @@ export function MySkills() {
         Programming Languages & Frameworks I Use
       </h2>
       <div className="bg-slate-800/50 border border-slate-800 rounded-2xl p-6 shadow-xl flex flex-wrap gap-2 text-xl">
-        {profile?.skills.map((skill, index) => (
-          <span
-            key={index}
-            className="px-2.5 py-1 bg-slate-800 border border-slate-700 text-slate-300 rounded-md font-mono">
-            {skill}
-          </span>
-        ))}
+        {deletingSkill && <p>Loading...</p>}
+
+        {!deletingSkill &&
+          profile?.skills.map((skill, index) => (
+            <span
+              key={index}
+              className="px-2.5 py-1 bg-slate-800 border border-slate-700 text-slate-300 rounded-md font-mono">
+              {skill}
+              <button
+                onClick={() => {
+                  handleDelete(skill);
+                }} className="cursor-pointer ml-2 relative -bottom-1">
+                <MdOutlineCancel />
+              </button>
+            </span>
+          ))}
+
         <AddSkillButton />
       </div>
     </div>
